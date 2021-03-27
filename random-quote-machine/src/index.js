dfc29e3457d5e6c0047fc969ca4c9f00a316219c
Mariana Pantelic <mari27line@yahoo.com>
2021-03-27 22:23:05 +0100

    Fetched the data

--- random-quote-machine/src/index.js
+++ random-quote-machine/src/index.js
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './main.css'
 import video from './components/Star-26094.mp4'
 
+
 const App = () => {
+
+    const [state, setState] = useState({quotes: []})
+
+    const getQuotes = () => {
+        return new Promise((resolve, reject) => {
+            fetch("https://type.fit/api/quotes")
+            .then((response) => {
+                if (response.status === 200) {
+                response
+                    .json()
+                    .then((data) => {
+                    resolve(data);
+                    })
+                    .catch((error) => {
+                    reject(error);
+                    });
+                } else {
+                reject(response.status);
+                }
+            })
+            .catch((error) => {
+                reject(error);
+            });
+        });
+    }
+
+    useEffect(() => {
+        getQuotes().then(data => {
+            setState({quotes: data})
+        })
+    },[])
+    
+    console.log(state.quotes)
+
+    const generateNewQuote = () => {
+        alert('hi')
+    }
+
     return (
-        <div>
-            <video autoPlay loop muted className='video'>
-                <source src={video} type="video/mp4"></source>
-            </video>
+        <>
+        <video autoPlay loop muted className='video'>
+            <source src={video} type="video/mp4"></source>
+        </video>
+        <div className="container">
+
+            <div className="row">
+                <div className="col-sm-8 mx-auto" id="quote-box">
+                    <h2 className="text-center" id="text"><i class="fas fa-quote-left"></i><i class="fas fa-quote-right"></i></h2>
+                    <p id="author"></p>
+                    <a href="twitter.com" id="tweet-quote"><i class="fab fa-twitter fa-2x"></i></a>
+                    <button className="btn bg-dark text-light" id="new-quote" onClick={generateNewQuote}>New Quote</button>
+                    
+                </div>
+            </div>
         </div>
+        </>
     );
 };
 
