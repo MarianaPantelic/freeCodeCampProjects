e3fc18aa8460e5f9ca483c804e1a48f965614d8d
Mariana Pantelic <mari27line@yahoo.com>
2021-04-05 12:11:37 +0200

    Made some changes

--- markdown-previewer/src/index.js
+++ markdown-previewer/src/index.js
@@ -1,19 +1,41 @@
-import React from 'react';
-import { Container, Row } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Col, Container, Form, Row, Badge } from 'react-bootstrap';
 import ReactDOM from 'react-dom';
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import MarkdownInput from './components/markdownInput';
-import Preview from './components/preview';
+let marked = require("marked");
 
 
 const App = () => {
+
+    const [state, setState] = useState({text: `# Markdown\n## Markdown\n[Check my code at:](https://github.com/MarianaPantelic/freeCodeCampProjects/tree/main/markdown-previewer)`})
+
+    const getText = (text) => {
+        console.log(text)
+        setState({text: text});
+    }
+    
+    marked(state.text)
+
     return (
         <Container>
             <h1 className="text-center mt-5">Markdown Previewer</h1>
-            <Row>
-                <MarkdownInput/>
-                <Preview/>
+            <Row className="mt-5">
+                <Col xs={12} md={12} lg={5} style={{border: "solid 2px black", height: "500px", margin: "10px"}}>
+                    <h2 className="text-center"><Badge variant="light">Markdown Input</Badge></h2>
+                    <Form>
+                        <Form.Group>
+                            <Form.Control as="textarea" rows={15} value={state.text} onChange={(e)=>getText(e.target.value)} id="editor">
+                                
+                            </Form.Control>
+                        </Form.Group>
+                    </Form>
+
+                </Col>
+                <Col xs={12} md={12} lg={5} style={{border: "solid 2px black", height: "500px", margin: "10px"}}>
+                    <h2 className="text-center"><Badge variant="light">Preview</Badge></h2>
+                    <div id="preview" dangerouslySetInnerHTML={{ __html: marked(state.text) }}></div>
+                </Col>
             </Row>
         </Container>
     );
