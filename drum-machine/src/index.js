8257a40495bbdbc5f37221fd147b164f440c8ee1
Mariana Pantelic <mari27line@yahoo.com>
2021-04-07 09:01:50 +0200

    Added sounds

--- drum-machine/src/index.js
+++ drum-machine/src/index.js
@@ -6,20 +6,57 @@ import './main.css'
 import { Col, Container, Row } from 'react-bootstrap';
 
 const App = () => {
+
+    const sounds = [
+        {
+            key: 'Q',
+            sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+        },
+        {
+            key: 'W',
+            sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+        },
+        {
+            key: 'E',
+            sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+        },
+        {
+            key: 'A',
+            sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+        },
+        {
+            key: 'S',
+            sound: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+        },
+        {
+            key: 'D',
+            sound: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+        },
+        {
+            key: 'Z',
+            sound: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+        },
+        {
+            key: 'X',
+            sound: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+        },
+        {
+            key: 'C',
+            sound: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+        },
+    ]
+
     return (
         <Container>
             <Row>
                 <Col lg={6} id="drum-machine">
                     <div id="display">
-                        <div className="drum-pad" id="">Q</div>
-                        <div className="drum-pad" id="">W</div>
-                        <div className="drum-pad" id="">E</div>
-                        <div className="drum-pad" id="">A</div>
-                        <div className="drum-pad" id="">S</div>
-                        <div className="drum-pad" id="">D</div>
-                        <div className="drum-pad" id="">Z</div>
-                        <div className="drum-pad" id="">X</div>
-                        <div className="drum-pad" id="">C</div>
+                       {sounds.map((elem, idx) => (
+                           <div className="drum-pad" id={idx}>
+                               {elem.key}
+                               <audio src={elem.sound} className='clip' id={elem.key}/>
+                           </div>
+                       ))} 
 
                     </div>
                 </Col>
