28fef3211d3f92a1e61e897179f3fc18f67a5be9
Mariana Pantelic <mari27line@yahoo.com>
2021-04-25 21:02:26 +0200

    Added backgroud image

---
+++ clock/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./main.css";
+
+const App = () => {
+  return (
+    <>
+      <div className="mainBg"></div>
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
